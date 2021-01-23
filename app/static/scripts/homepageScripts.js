828b0627f9299678f0267a4b65ed14d9a694d17c
James Payne <james.payne1349@gmail.com>
2021-01-22 21:38:43 -0800

    fixes for the .offset method that was screwing up the page load

--- app/static/scripts/homepageScripts.js
+++ app/static/scripts/homepageScripts.js
@@ -1,76 +1,101 @@
 
 
-var image_container = $('#image_container');
-
-// we want to know a few things, so we can set the pictures to the right size..
-var window_height = document.documentElement.clientHeight;
 
 
-var container_pos = image_container.offset();
-console.log(container_pos.top, container_pos.left);
+console.log(document.fonts.ready);
 
-// use the bottom of the header, and the page size, to calculate container height.
-var container_height = window_height - container_pos.top;
+var fontsReadyPromise = document.fonts.ready;
 
-image_container.css({
-    'width':'100vw',
-    'height': container_height + 'px',
-});
+fontsReadyPromise.then(
+    function(value) { addImage(); console.log(value);}
+);
 
-// declaration of new elements
-var shadow_container = document.createElement('div');
-var image_one = new Image();
+//  call this when the 'web font' has loaded..
+// GOTCHA. now we could brake this up  alitte? unsure.
+function addImage() {
 
-if( window_width > 768 ) {
-    $(shadow_container).css({
-        'z-index':'3',
-        'height':container_height + 'px',
-        'width':'100%',
-        'background-color':'rgba(0,0,0,0)',
-        'box-shadow':'0 70px 30px -30px white inset',
-        'position':'absolute',
-        'top':container_pos.top - 2 + 'px'
+    let image_container = $('#image_container');
+
+
+    // we want to know a few things, so we can set the pictures to the right size..
+    var window_height = document.documentElement.clientHeight;
+
+
+    // our problem is calling this .offset function before the actual font is loaded in..
+    var container_pos = image_container.offset();
+    // console.log(container_pos.top, container_pos.left);
+
+    // use the bottom of the header, and the page size, to calculate container height.
+    var container_height = window_height - container_pos.top;
+
+    image_container.css({
+        'width':'100vw',
+        'height': container_height + 'px',
     });
-    
-    $(image_one).css({
-    
-    'width':'100vw',
-    'height': container_height + 'px',
-    'object-fit':'cover',
-    'object-position':'50% 80%'
 
-}); 
-} else { // mobile view
-    $(shadow_container).css({
-        'z-index':'3',
-        'height':container_height + 'px',
-        'width':'100%',
-        'background-color':'rgba(0,0,0,0)',
-        'box-shadow':'0 100px 30px -30px white inset',
-        'position':'absolute',
-        'top':container_pos.top - 2 + 'px'
-    });
-    
-    $(image_one).css({
-    
-    'width':'100vw',
-    'height': container_height + 'px',
-    'object-fit':'cover',
-    'object-position':'50% 100%',
-    'display': 'none'
-}); 
-}
+    // declaration of new elements
+    var shadow_container = document.createElement('div');
+    var image_one = new Image();
 
-image_container.append(shadow_container)
+    image_one.id = 'homepage_image';
 
-image_one.onload = function () {
-    image_container.append(image_one);
-    image_one.style.display = 'block';
+    if( window_width > 768 ) {
+        $(shadow_container).css({
+            'z-index':'3',
+            'height':container_height + 'px',
+            'width':'100%',
+            'background-color':'rgba(0,0,0,0)',
+            'box-shadow':'0 70px 30px -30px white inset',
+            'position':'absolute',
+            'top':container_pos.top - 2 + 'px'
+        });
+        
+        $(image_one).css({
+        
+        'width':'100vw',
+        'height': container_height + 'px',
+        'object-fit':'cover',
+        'object-position':'50% 80%',
 
+    }); 
+    } else { // mobile view
+        $(shadow_container).css({
+            'z-index':'3',
+            'height':container_height + 'px',
+            'width':'100%',
+            'background-color':'rgba(0,0,0,0)',
+            'box-shadow':'0 100px 30px -30px white inset',
+            'position':'absolute',
+            'top':container_pos.top - 2 + 'px'
+        });
+        
+        $(image_one).css({
+        
+        'width':'100vw',
+        'height': container_height + 'px',
+        'object-fit':'cover',
+        'object-position':'50% 100%',
+        
+    }); 
+    }
+
+
+
+    image_one.onload = function(ev) {
+        image_container.append(shadow_container)
+        image_container.append(image_one);
+        console.log(ev);
+        setTimeout(function() {
+            let image = $('#homepage_image');
+            image.css('opacity', '1');
+        }, 200);
+
+        };
+
+    image_one.error = function () {
+    console.log('ERROR!');
     };
 
-image_one.error = function () {
-  console.log('ERROR!');
-};
+    image_one.src = '/static/engagement_one.jpg';
 
-image_one.src = '/static/engagement_one.jpg';
+}
